a520f5b6def5c86c930274671a412d272ffe3a01
yeswanthsai.inturi <yeswanthsai.inturi@gmail.com>
2024-09-14 10:35:58 +0000

    changes for css media

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,12 +1,4 @@
-/**
- * @license MIT
- * @fileoverview All module functions
- * @copyright Yeswanth Sai Inturi 2024 All rights reserved
- * @author Yeswanth Sai Inturi <yeswanthsai.inturi@gmail.com>
- >
- */
-
-"use strict";
+"ues strict";
 
 export const weekDayNames = [
   "Sunday",
@@ -19,29 +11,27 @@ export const weekDayNames = [
 ];
 
 export const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
   "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
 ];
 
 /**
- *
- * @param {number} dataUnix Unix date in seconds
- * @param {number} timezone Timezone shift for UTC in seconds
- * @returns {string} Date String. formate: "Sunday 10, Jan"
+ * @param {number} dateUnix  Unix date in seconds
+ * @param {number} timezone  Timezone shift from UTC in seconds
+ * @returns {string} Date String. formate: "Weekday 10, Jan"
  */
-
-export const getData = function (dataUnix, timezone) {
-  const date = new Date((dataUnix + timezone) * 1000);
+export const getDate = function (dateUnix, timezone) {
+  const date = new Date((dateUnix + timezone) * 1000);
   const weekDayName = weekDayNames[date.getUTCDay()];
   const monthName = monthNames[date.getUTCMonth()];
 
@@ -49,13 +39,25 @@ export const getData = function (dataUnix, timezone) {
 };
 
 /**
- *
- * @param {number} timeUnix Unix date in seconds
- * @param {number} timezone TimeZone shift from UTC in sec
- * @returns {string} Time String. format: "HH AM/PM"
+ * @param {number} timeUnix Unix time in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time String. formate: "HH:MM AM/PM"
  */
-
 export const getTime = function (timeUnix, timezone) {
+  const date = new Date((timeUnix + timezone) * 1000);
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const period = hours >= 12 ? "PM" : "AM";
+
+  return `${hours % 12 || 12}:${minutes} ${period}`;
+};
+
+/**
+ * @param {number} timeUnix Unix time in seconds
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time String. formate: "HH AM/PM"
+ */
+export const getHours = function (timeUnix, timezone) {
   const date = new Date((timeUnix + timezone) * 1000);
   const hours = date.getUTCHours();
   const period = hours >= 12 ? "PM" : "AM";
@@ -64,10 +66,38 @@ export const getTime = function (timeUnix, timezone) {
 };
 
 /**
- *
- * @param {number} mps Meter Per seconds
- * @returns {number} Kilometer per hours
+ * @param {number} mps Metter per seconds
+ * @returns {number} Kilometer per hour
  */
-export const mpsToKmph = function (mps) {
-  return (mps * 3.6).toFixed(2);
+export const mps_to_kmh = (mps) => {
+  const mph = mps * 3600;
+  return mph / 1000;
+};
+
+export const apiText = {
+  1: {
+    level: "Good",
+    message:
+      "Air quality is considered satisfactory, and air pollution poses little or no risk",
+  },
+  2: {
+    level: "Fair",
+    message:
+      "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.",
+  },
+  3: {
+    level: "Moderate",
+    message:
+      "Members of sensitive groups may experience health effects. The general public is not likely to be affected.",
+  },
+  4: {
+    level: "Poor",
+    message:
+      "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.",
+  },
+  5: {
+    level: "Very Poor",
+    message:
+      "Health warnings of emergency conditions. The entire population is more likely to be affected.",
+  },
 };

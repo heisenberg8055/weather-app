37edf5314fe0bdc615488354419c05606cb060b3
yeswanthsai.inturi <yeswanthsai.inturi@gmail.com>
2024-09-14 07:08:27 +0000

    js modules

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,44 @@
 /**
  * @license MIT
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @copyright Yeswanth Sai Inturi 2024 All rights reserved
+ * @author Yeswanth Sai Inturi <yeswanthsai.inturi@gmail.com>
  */
+
+"use strict";
+
+const api_key = "71b14ece60beab8353719aa5af855e3c";
+
+/**
+ * 
+ * @param {string} URL API URL
+ * @param {function} callback callback function
+ */
+
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => console.log(error));
+};
+
+export const url = {
+    currentWeather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airpollution(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon){
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * 
+     * @param {string} city Search Query e.g.: "London", "New York"
+     */
+    geo(city){
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5`
+    }
+}
--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,73 @@
 /**
  * @license MIT
  * @fileoverview All module functions
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * @copyright Yeswanth Sai Inturi 2024 All rights reserved
+ * @author Yeswanth Sai Inturi <yeswanthsai.inturi@gmail.com>
+ >
  */
+
+"use strict";
+
+export const weekDayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+/**
+ *
+ * @param {number} dataUnix Unix date in seconds
+ * @param {number} timezone Timezone shift for UTC in seconds
+ * @returns {string} Date String. formate: "Sunday 10, Jan"
+ */
+
+export const getData = function (dataUnix, timezone) {
+  const date = new Date((dataUnix + timezone) * 1000);
+  const weekDayName = weekDayNames[date.getUTCDay()];
+  const monthName = monthNames[date.getUTCMonth()];
+
+  return `${weekDayName} ${date.getUTCDate()}, ${monthName}`;
+};
+
+/**
+ *
+ * @param {number} timeUnix Unix date in seconds
+ * @param {number} timezone TimeZone shift from UTC in sec
+ * @returns {string} Time String. format: "HH AM/PM"
+ */
+
+export const getTime = function (timeUnix, timezone) {
+  const date = new Date((timeUnix + timezone) * 1000);
+  const hours = date.getUTCHours();
+  const period = hours >= 12 ? "PM" : "AM";
+
+  return `${hours % 12 || 12} ${period}`;
+};
+
+/**
+ *
+ * @param {number} mps Meter Per seconds
+ * @returns {number} Kilometer per hours
+ */
+export const mpsToKmph = function (mps) {
+  return (mps * 3.6).toFixed(2);
+};
